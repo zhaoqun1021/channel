0d47426fd015609adfcb6e6a2734dd01a4bbc711
zhaoqun <911@zhaoqun911.cn>
2016-01-14 01:39:37 +0800

    cc

---
+++ src/main/java/cn/zhaoqun911/channel/App.java
@@ -0,0 +1,13 @@
+package cn.zhaoqun911.channel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
